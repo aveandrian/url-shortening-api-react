8697ac567fb1d10c7fdbce40c05a22710667a45c
aveandrian <trancemilly7@gmail.com>
2023-10-15 20:52:31 +0200

    fix shortening request

--- src/components/LinkShortener.jsx
+++ src/components/LinkShortener.jsx
@@ -1,66 +1,94 @@
-import { useEffect, useState } from "react"
-import { nanoid } from "nanoid"
-import '../styles/LinkShortener.css'
-import SuggestedLink from "./SuggestedLink"
+import { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
+import "../styles/LinkShortener.css";
+import SuggestedLink from "./SuggestedLink";
+import axios from "axios";
 
-export default function LinkShortener(){
-    const [linkInput, setLinkInput] = useState('')
-    const [error, setError] = useState(null)
-    const [shortLinksList, setShortLinksList] = useState(null)
+export default function LinkShortener() {
+  const [linkInput, setLinkInput] = useState("");
+  const [error, setError] = useState(null);
+  const [shortLinksList, setShortLinksList] = useState(null);
+  const TOKEN = "jyIhoMzE4jvhhmlnaOgrs2jRTRGf4yoraYeRlHDhaoRRpDtWi8YGX6WlEKz7";
 
-    useEffect(()=>{
-        let savedLinks = window.localStorage.getItem('shortenedLinks')
-        if(JSON.parse(savedLinks)){
-            setShortLinksList(JSON.parse(savedLinks))
+  useEffect(() => {
+    let savedLinks = window.localStorage.getItem("shortenedLinks");
+    if (JSON.parse(savedLinks)) {
+      setShortLinksList(JSON.parse(savedLinks));
+    } else setShortLinksList([]);
+  }, []);
+
+  useEffect(() => {
+    if (shortLinksList)
+      window.localStorage.setItem(
+        "shortenedLinks",
+        JSON.stringify(shortLinksList)
+      );
+  }, [shortLinksList]);
+
+  function handleChange(e) {
+    setError(null);
+    setLinkInput(e.target.value);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (linkInput == "" || !isValidURL(linkInput))
+      return setError("Please add a valid link");
+
+    try {
+      const { data } = await axios.post(
+        `https://api.tinyurl.com/create?api_token=${TOKEN}`,
+        {
+          url: linkInput,
         }
-        else setShortLinksList([])
-    }, [])
-
-    useEffect(()=>{
-        if(shortLinksList)
-            window.localStorage.setItem('shortenedLinks', JSON.stringify(shortLinksList))
-    }, [shortLinksList])
-
-    function handleChange(e){
-        setError(null)
-        setLinkInput(e.target.value)
+      );
+      setShortLinksList((prev) => [
+        ...prev,
+        {
+          id: nanoid(),
+          fullLink: linkInput,
+          shortLink: data.data.tiny_url,
+        },
+      ]);
+    } catch (err) {
+      setError(err);
     }
+    setLinkInput("");
+  }
 
-    function handleSubmit(e){
-        e.preventDefault()
-        if(linkInput == '' || !isValidURL(linkInput))
-            return setError('Please add a valid link')
-        
-        fetch(`https://api.shrtco.de/v2/shorten?url=${linkInput}`)
-        .then(res => res.json())
-        .then(body => setShortLinksList(prev => [...prev, {
-                id: nanoid(),
-                fullLink: body.result.original_link,
-                shortLink: body.result.full_short_link
-            }
-        ]))
-        setLinkInput('')
+  function isValidURL(str) {
+    var httpRegex =
+      /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
+    if (httpRegex.test(str)) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    function isValidURL(str) {
-        var httpRegex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
-        if(httpRegex.test(str)) {
-            return true
-         } else {
-            return false
-         }
-     }
-
-    return (
-        <div className="link-shortener-container">
-            <div className="link-shortner-wrapper">
-                <form onSubmit={handleSubmit} className="link-shortener-form">
-                    <input className={`link-input ${error ? "input-error" : ""}`} type="text" placeholder="Shorten a link here..." value={linkInput} onChange={handleChange}/>
-                    <input className="shorten-btn" type="submit"  value='Shorten it!'></input>
-                </form>
-                {error && <p className="error">{error}</p>}
-            </div>
-            {shortLinksList && shortLinksList.map(shortLinkListItem => <SuggestedLink key={shortLinkListItem.id} {...shortLinkListItem} />)}
-        </div>
-    )
+  return (
+    <div className="link-shortener-container">
+      <div className="link-shortner-wrapper">
+        <form onSubmit={handleSubmit} className="link-shortener-form">
+          <input
+            className={`link-input ${error ? "input-error" : ""}`}
+            type="text"
+            placeholder="Shorten a link here..."
+            value={linkInput}
+            onChange={handleChange}
+          />
+          <input
+            className="shorten-btn"
+            type="submit"
+            value="Shorten it!"
+          ></input>
+        </form>
+        {error && <p className="error">{error}</p>}
+      </div>
+      {shortLinksList &&
+        shortLinksList.map((shortLinkListItem) => (
+          <SuggestedLink key={shortLinkListItem.id} {...shortLinkListItem} />
+        ))}
+    </div>
+  );
 }
